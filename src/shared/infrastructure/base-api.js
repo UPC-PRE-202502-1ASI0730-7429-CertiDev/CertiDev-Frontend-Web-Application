8b22aa7fede1cfae2ea1fb31ce1907f83cbee343
Joaquin Carranza <119025927+thepima@users.noreply.github.com>
2025-12-06 09:48:35 -0500

    Change API base URL to remote JSON server

--- src/shared/infrastructure/base-api.js
+++ src/shared/infrastructure/base-api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: 'https://my-json-server.typicode.com/UPC-PRE-202502-1ASI0730-7429-CertiDev/db.json',
     headers: { 'Content-Type': 'application/json' },
 });
 
